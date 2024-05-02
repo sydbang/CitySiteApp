a44bc62bd6bae2eb20da82de9d7380b7e0046583
Sydney Bang <sydney.bang94@gmail.com>
2024-05-02 12:59:04 -0700

    initial commit

---
+++ CitySiteApp/CitySiteApp/CitySiteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CitySiteAppApp.swift
+//  CitySiteApp
+//
+//  Created by Sunghee Bang on 2024-05-02.
+//
+
+import SwiftUI
+
+@main
+struct CitySiteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CitySiteApp/CitySiteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CitySiteApp
+//
+//  Created by Sunghee Bang on 2024-05-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
